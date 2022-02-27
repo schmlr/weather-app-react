2757d27f9a6aad35138f1e5c8f2d4f1de86114e1
schmlr <98670792+schmlr@users.noreply.github.com>
2022-02-27 11:47:36 +0100

    Remove unused variables

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from "./Search.js";
 import Weather from "./Weather.js";
-import reportWebVitals from './reportWebVitals';
 
 import "./weather-app.css";
 
