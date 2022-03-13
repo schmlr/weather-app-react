a2b234fea0c46947933cdcad2789b05fe49627b7
schmlr <98670792+schmlr@users.noreply.github.com>
2022-03-13 19:15:15 +0100

    Include date component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,9 +1,44 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-    let day = props.date.getDay();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+        let months = [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ];
+    let day = days[props.date.getDay()];
+    let date = props.date.getDate();
     let hours = props.date.getHours();
-    let minutes = props.date.GetMinutes();
+    let minutes = props.date.getMinutes();
+    let month = months[props.date.getMonth()];
 
-    return
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return (
+        `${day}, ${month} ${date}, ${hours}:${minutes}`
+    )
 }
